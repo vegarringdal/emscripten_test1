e24a5e7c338672acdd3d42e5b8838a9bcf46667a
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-06 23:27:18 +0200

    save

--- test.js
+++ test.js
@@ -3,20 +3,28 @@ const { performance } = require("perf_hooks");
 
 m.onRuntimeInitialized = () => {
   // create array
-  const length = 5000;
+  const length = 2000000;
   const data = new Float32Array(length);
   const bytes = data.length + data.BYTES_PER_ELEMENT;
 
   // pointer
   const t1 = performance.now();
   const dataPtr = m._malloc(bytes);
+  const usedTime1 = performance.now() - t1;
 
   // allocate/copy
-  const dataHeap = new Uint32Array(m.HEAPU32.buffer, dataPtr, bytes);
-  dataHeap.set(new Uint32Array(data.buffer));
+  const dataHeap = new Float32Array(m.HEAPU32.buffer, dataPtr, bytes);
 
+  const usedTime2 = performance.now() - t1;
+
+  dataHeap.set(new Float32Array(data.buffer));
+
+  const usedTime3 = performance.now() - t1;
+  
   // call
   m._fill_float_array(dataHeap.byteOffset, length);
+
+  const usedTime4 = performance.now() - t1;
 
   // get result
   const result = new Float32Array(
@@ -24,7 +32,15 @@ m.onRuntimeInitialized = () => {
     dataHeap.byteOffset,
     data.length
   );
-  console.log(result, performance.now() - t1);
+
+  const usedTime5 = performance.now() - t1;
+
+  console.log(result.length);
+  console.log(usedTime1);
+  console.log(usedTime2);
+  console.log(usedTime3);
+  console.log(usedTime4);
+  console.log(usedTime5);
 
   // Free memory
   m._free(dataHeap.byteOffset);
