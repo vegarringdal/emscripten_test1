de77c7a8b0f18ead26052a6c7b2ab93925be5128
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-10 14:22:03 +0200

    max 2 gig

--- test.js
+++ test.js
@@ -3,9 +3,12 @@ const { performance } = require("perf_hooks");
 
 m.onRuntimeInitialized = () => {
   // create array
-  const length = 2000000;
+  const length = 500_000_000; // ALLOW_MEMORY_GROWTH -s MAXIMUM_MEMORY=4GB looks like these dont do anything.. probably will never need it, but still.
   const data = new Float32Array(length);
-  const bytes = data.length + data.BYTES_PER_ELEMENT;
+  const bytes = data.length * data.BYTES_PER_ELEMENT;
+  console.log(4_294_967_295 - bytes);
+  console.log(bytes / 1_000_000_000, "GB");
+  console.log(4_294_967_295 < bytes);
 
   // pointer
   const t1 = performance.now();
@@ -13,14 +16,14 @@ m.onRuntimeInitialized = () => {
   const usedTime1 = performance.now() - t1;
 
   // allocate/copy
-  const dataHeap = new Float32Array(m.HEAPU32.buffer, dataPtr, bytes);
+  const dataHeap = new Uint8Array(m.HEAP8.buffer, dataPtr, bytes);
 
   const usedTime2 = performance.now() - t1;
 
-  dataHeap.set(new Float32Array(data.buffer));
+  dataHeap.set(new Uint8Array(data.buffer));
 
   const usedTime3 = performance.now() - t1;
-  
+
   // call
   m._fill_float_array(dataHeap.byteOffset, length);
 
@@ -34,7 +37,7 @@ m.onRuntimeInitialized = () => {
   );
 
   const usedTime5 = performance.now() - t1;
-
+  console.log(result);
   console.log(result.length);
   console.log(usedTime1);
   console.log(usedTime2);
