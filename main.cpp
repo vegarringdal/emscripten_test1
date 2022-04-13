2d18b99abc31eef20ca8989eeace8619a8319a80
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-13 10:57:41 +0200

    add parts I need to use sumplify (not tested...)

--- main.cpp
+++ main.cpp
@@ -1,25 +1,33 @@
 
-#include "Modifier.h"
+#include "Simplify.h"
 
 extern "C"
 {
     void read(int *index, int indexSize, float *positions, int positionSize)
     {
-        Modifier::read(index, indexSize, positions, positionSize);
+        Simplify::read(index, indexSize, positions, positionSize);
+        double agressiveness = 7.0;
+        int target_count = 10;
+        Simplify::simplify_mesh(target_count, agressiveness, true);
+        // todo: error checking
+        // I prob should return int for success/error + new sizes?
+        // or just own function for status ?
     }
+   
 
     void fill(int *index, float *positions)
     {
-        Modifier::fill(index, positions);
+        // this prob could have been done in the read part
+        Simplify::fill(index, positions);
     }
 
     int getTriangeSize()
     {
-        return Modifier::getTriangeSize();
+        return Simplify::getTriangeSize();
     }
 
     int getVertexSize()
     {
-        return Modifier::getVertexSize();
+        return Simplify::getVertexSize();
     }
 }
